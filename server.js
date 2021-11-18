ef3eaf3a3db9b79e740286560242883bf498f5f2
Christian Colson <christiancolson91@gmail.com>
2021-11-18 16:32:14 -0600

    added html

--- server.js
+++ server.js
@@ -3,10 +3,26 @@ const PORT = process.env.PORT || 6001
 
 // dependencies 
 const express = require('express')
-const server = express()
+const app = express()
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '3643f04951734db58d69c2ff01abad2c',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
 
+// record generic message and send to rollbar
+rollbar.log('Hello world!')
 
-
+app.get('/', (req, res) => {
+  try{
+    nonExistentFunction()
+  } catch (error) {
+    rollbar.info('That function does not exist')
+    res.send('Someone tapped the API')
+  }
+})
 
 
 
